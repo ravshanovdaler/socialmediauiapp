f1f2e4bd7ee0f7b6913f9d5f17142181694ccc77
Daler Ravshanov <ravshanovdaler06@gmail.com>
2023-12-10 15:51:08 +0500

    Home page

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   List<String> profileImages = [
+    'images/5.jpg',
     'images/1.jpg',
     'images/2.jpg',
     'images/3.jpg',
     'images/4.jpg',
-    'images/5.jpg',
   ];
 
   @override
@@ -58,27 +58,66 @@ class MyApp extends StatelessWidget {
             IconButton(onPressed: () {}, icon: Icon(Icons.chat_bubble_outline)),
           ],
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: List.generate(
-              5,
+        body: SingleChildScrollView ( child:
+        Column(
+          children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                  5,
                   (index) => Container(
-                margin: EdgeInsets.all(5),
-                child: Column(
-                  children: <Widget>[
-                    CircleAvatar(
-                      backgroundImage: AssetImage(profileImages[index]),
-                      radius: 35,
+                    margin: EdgeInsets.all(5),
+                    child: Column(
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: AssetImage(profileImages[index]),
+                          radius: 35,
+                        ),
+                        Text("Profile name")
+                      ],
                     ),
-                    Text("Profile name")
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
+            // Add other widgets below the SingleChildScrollView
+            Divider(),
+            Column(
+              children: List.generate(
+                  5,
+                  (index) => Column(
+                      children:<Widget>[
+                          Row(children: <Widget>[
+                            Container (
+                              padding: EdgeInsets.all(10),
+                              child: CircleAvatar(
+                                backgroundImage:
+                                    AssetImage(profileImages[index]),
+                              ),
+                            ),
+                            Text('Profile name'),
+                            Spacer(),
+                            IconButton(
+                                onPressed: () {}, icon: Icon(Icons.more_vert))
+                          ]),
+                          Image.asset(profileImages[index]),
+                          Row(
+                            children: <Widget>[
+                              IconButton(onPressed: (){}, icon: Icon(Icons.favorite_border)),
+                              IconButton(onPressed: (){}, icon: Icon(Icons.message)),
+                              IconButton(onPressed: (){}, icon: Icon(Icons.ios_share)),
+                              Spacer(),
+                              IconButton(onPressed: (){}, icon: Icon(Icons.bookmark_outline)),
+                            ],
+                          ),
+                          Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et')
+                        ],
+                      )),
+            )
+            ],
         ),
       ),
-    );
+      ));
   }
 }
